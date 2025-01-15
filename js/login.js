01c21ba06e39847229e65d010efe83bde99000e3
WoodlandTea5 <102604967+WoodlandTea5@users.noreply.github.com>
2025-01-15 10:13:17 -0300

    Update login.js
    Mudei o if do e-mail e senha para que a validação seja do que o usuário digitou no cadastro

--- js/login.js
+++ js/login.js
@@ -1,9 +1,15 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    e.preventDefault(); 
 
-    if (email === "admin@debroi.com" && password === "123456") {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+    
+    if (email && password) {
         alert("Login realizado com sucesso!");
         window.location.href = "/html/agendamento.html";
     } else {
